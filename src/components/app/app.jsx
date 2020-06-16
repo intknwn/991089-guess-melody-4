91ed5e5048022a3e13a245d863d7afb2abe85dec
intknwn <intknwn@gmail.com>
2020-06-16 20:04:08 +0800

    Рефакторит код

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const welcomeButtonClickHandler = () => {};
-
 const App = (props) => {
   const {errorsCount} = props;
-  return <WelcomeScreen
-    onWelcomeButtonClick={welcomeButtonClickHandler}
-    errorsCount={errorsCount}
-  />;
+  return (
+    <WelcomeScreen
+      onWelcomeButtonClick={() => {}}
+      errorsCount={errorsCount}
+    />
+  );
 };
 
 App.propTypes = {
